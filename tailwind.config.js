d723009b164d891842ca88f8237be405bef39c3e
Garrison Snelling <garrison@speedbumpit.com>
2020-08-31 23:05:00 -0500

    ADding in new header and nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
   purge: ["src/**/*.{js,jsx,ts,tsx}","src/**/**/*.{js,jsx,ts,tsx}","src/**/**/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
