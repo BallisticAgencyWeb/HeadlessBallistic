217b918c404e3c90ad2bd9145df02211b9a2a2a2
Garrison Snelling <garrison@speedbumpit.com>
2020-09-02 23:34:20 -0500

    Making the tagline text larger

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -91,7 +91,7 @@ export const IndexPageTemplate = ({
             <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
               {mainpitch.title}
             </h2>
-            <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+            <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-2xl md:max-w-3xl">
               {mainpitch.subtitle}
             </p>
             <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
