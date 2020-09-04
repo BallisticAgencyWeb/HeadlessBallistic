b7b36b603ae32dab649d28c2e93a85443f80d3e9
Garrison Snelling <garrison@speedbumpit.com>
2020-09-04 16:13:39 -0500

    Fixing issue in safari

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -9,22 +9,22 @@ function Customers() {
           Trusted by industry leaders:
         </p>
         <div className="mt-6 grid grid-cols-2 gap-8 md:grid-cols-6">
-          <div className="col-span-1 flex justify-center">
+          <div className="col-span-1">
             <img src="https://cdn11.bigcommerce.com/s-b7clv5baib/stencil/34206090-cdcb-0138-6ba8-0242ac110007/e/5327cb50-be27-0138-ce8e-0242ac11000d/img/logos/company-logos-02.png" className=" ls-is-cached lazyloaded" />
           </div>
-          <div className="col-span-1 flex justify-center">
+          <div className="col-span-1">
             <img src="https://cdn11.bigcommerce.com/s-b7clv5baib/stencil/34206090-cdcb-0138-6ba8-0242ac110007/e/5327cb50-be27-0138-ce8e-0242ac11000d/img/logos/company-logos-03.png" className=" ls-is-cached lazyloaded" />
           </div>
-          <div className="col-span-1 flex justify-center">
+          <div className="col-span-1">
             <img src="https://cdn11.bigcommerce.com/s-b7clv5baib/stencil/34206090-cdcb-0138-6ba8-0242ac110007/e/5327cb50-be27-0138-ce8e-0242ac11000d/img/logos/company-logos-04.png" className=" ls-is-cached lazyloaded" />
           </div>
-          <div className="col-span-1 flex justify-center">
+          <div className="col-span-1">
             <img src="https://cdn11.bigcommerce.com/s-b7clv5baib/stencil/34206090-cdcb-0138-6ba8-0242ac110007/e/5327cb50-be27-0138-ce8e-0242ac11000d/img/logos/company-logos-05.png" className=" ls-is-cached lazyloaded" />
           </div>
-          <div className="col-span-1 flex justify-center">
+          <div className="col-span-1">
             <img src="https://cdn11.bigcommerce.com/s-b7clv5baib/stencil/34206090-cdcb-0138-6ba8-0242ac110007/e/5327cb50-be27-0138-ce8e-0242ac11000d/img/logos/company-logos-07.png" className=" ls-is-cached lazyloaded" />
           </div>
-          <div className="col-span-1 flex justify-center">
+          <div className="col-span-1">
             <img src="https://cdn11.bigcommerce.com/s-b7clv5baib/stencil/34206090-cdcb-0138-6ba8-0242ac110007/e/5327cb50-be27-0138-ce8e-0242ac11000d/img/logos/company-logos-08.png" className=" ls-is-cached lazyloaded" />
           </div>
         </div>
