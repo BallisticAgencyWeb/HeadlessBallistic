712cd1740ac4049db2cc91fccfb9e943578fe645
Garrison Snelling <garrison@speedbumpit.com>
2020-09-08 10:22:18 -0500

    Updating products template

--- src/cms/preview-templates/ProductPagePreview.js
+++ src/cms/preview-templates/ProductPagePreview.js
@@ -5,10 +5,7 @@ import { ProductPageTemplate } from '../../templates/product-page'
 const ProductPagePreview = ({ entry, getAsset }) => {
   return (
     <ProductPageTemplate
-      image={entry.getIn(['data', 'image'])}
       title={entry.getIn(['data', 'title'])}
-      heading={entry.getIn(['data', 'heading'])}
-      description={entry.getIn(['data', 'description'])}
     />
   )
 }
