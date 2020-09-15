93f03d3b37c3721853ba7af4015a130bc6e03831
Garrison Snelling <garrison@speedbumpit.com>
2020-09-15 10:16:44 -0500

    Adding in secondary pages

---
+++ src/templates/secondary-post.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { kebabCase } from 'lodash'
+import Helmet from 'react-helmet'
+import { graphql, Link } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
+
+export const SecondaryPostTemplate = ({
+  content,
+  contentComponent,
+  title,
+  helmet,
+}) => {
+  const PostContent = contentComponent || Content
+
+  return (
+    <section className="section">
+      {helmet || ''}
+      <div className="container content">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+              {title}
+            </h1>
+            <PostContent content={content} />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+SecondaryPostTemplate.propTypes = {
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  title: PropTypes.string,
+  helmet: PropTypes.object,
+}
+
+const SecondaryPost = ({ data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <Layout>
+      <SecondaryPostTemplate
+        content={post.html}
+        contentComponent={HTMLContent}
+        helmet={
+          <Helmet titleTemplate="%s | Page">
+            <title>{`${post.frontmatter.title}`}</title>
+            <meta
+              name="description"
+              content={`${post.frontmatter.description}`}
+            />
+          </Helmet>
+        }
+        title={post.frontmatter.title}
+      />
+    </Layout>
+  )
+}
+
+SecondaryPost.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
+}
+
+export default SecondaryPost
+
+export const pageQuery = graphql`
+  query SecondaryPostByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        description
+        tags
+      }
+    }
+  }
+`
