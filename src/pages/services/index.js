a64bdfcf68699478576943cd4114060dc566a10d
Garrison Snelling <garrison@speedbumpit.com>
2020-09-21 16:18:47 -0500

    Update index.js

--- src/pages/services/index.js
+++ src/pages/services/index.js
@@ -14,7 +14,7 @@ export default class ServicesIndexPage extends React.Component {
           <div className="relative max-w-lg mx-auto lg:max-w-7xl">
             <div>
               <h2 className="text-3xl leading-9 tracking-tight font-extrabold text-gray-900 sm:text-3xl sm:leading-10">
-                Services
+                Optimized eCommerce™ Services
               </h2>
             </div>
             <ServiceRoll />
