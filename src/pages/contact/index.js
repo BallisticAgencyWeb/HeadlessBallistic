32ec29acb795405e3661869f673c1b55fb713579
Garrison Snelling <garrison@speedbumpit.com>
2020-09-22 16:29:27 -0500

    ADjusting formatting of the contact page

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,35 +1,30 @@
-import React from 'react'
-import Layout from '../../components/Layout'
-import ContactForm from '../../components/ContactForm';
-import PageHeading from '../../components/PageHeading';
-
-
+import React from "react";
+import Layout from "../../components/Layout";
+import ContactForm from "../../components/ContactForm";
+import PageHeading from "../../components/PageHeading";
 
 export default class Index extends React.Component {
-
   render() {
     return (
       <Layout>
-        <div className="container is-fluid">
-          <section className="section">
-            <div className="container">
-              <h1 className="text-2xl text-center font-bold text-gray-900 sm:text-5xl sm:leading-snug sm:truncate mb-10">Contact</h1>
-              <div className="columns">
-                <div className="column is-6">
-                  <img className="rounded-md shadow-xl" src="https://images.unsplash.com/photo-1517520688650-28eb9304f326?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=628&q=80" alt="" />
-                </div>
-                <div className="column is-5 is-offset-1">
-                  <div className="container">
-                    <div className="content">
-                      <ContactForm />
-                    </div>
-                  </div>
-                </div>
-              </div>
+        <div className="container w-full px-4 sm:px-8 mb-10">
+          <h1 className='text-2xl text-center font-bold text-gray-900 sm:text-5xl sm:leading-snug sm:truncate my-5 sm:my-10'>
+            Contact
+          </h1>
+          <div className='sm:flex'>
+            <div className='hidden sm:flex w-full mt-8'>
+              <img
+                className='rounded-md shadow-xl'
+                src='https://images.unsplash.com/photo-1517520688650-28eb9304f326?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=628&q=80'
+                alt=''
+              />
             </div>
-          </section>
+            <div className='w-full mt-8 sm:ml-8'>
+              <ContactForm />
+            </div>
+          </div>
         </div>
       </Layout>
-    )
+    );
   }
 }
